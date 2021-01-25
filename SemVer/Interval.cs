029b481597fb4ff52e186be8965ec30cefd06191
Евгений Сапожников <jenya.sapozhnickov@yandex.ru>
2021-01-25 11:34:05 +0300

    Добавьте файлы проекта.

---
+++ SemVer/Interval.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace SemVer
+{
+    public class Interval
+    {
+        private const string versionChars = @"[0-9a-zA-Z\-\+\.\*]";
+
+        public static Tuple<int, Comparator[]> CaretRange(string spec)
+        {
+            string pattern = String.Format(@"^\s*\^\s*({0}+)\s*", versionChars);
+
+            var regex = new Regex(pattern);
+            var match = regex.Match(spec);
+            if (!match.Success)
+            {
+                return null;
+            }
+
+            Version minVersion = null;
+            Version maxVersion = null;
+
+            var version = new PartialVersion(match.Groups[1].Value);
+
+            if (version.Major.Value > 0)
+            {
+                // Don't allow major version change
+                minVersion = version.ToZeroVersion();
+                maxVersion = new Version(version.Major.Value + 1, 0, 0);
+            }
+            else if (!version.Minor.HasValue)
+            {
+                // Don't allow major version change, even if it's zero
+                minVersion = version.ToZeroVersion();
+                maxVersion = new Version(version.Major.Value + 1, 0, 0);
+            }
+            else if (!version.Patch.HasValue)
+            {
+                // Don't allow minor version change, even if it's zero
+                minVersion = version.ToZeroVersion();
+                maxVersion = new Version(0, version.Minor.Value + 1, 0);
+            }
+            else if (version.Minor > 0)
+            {
+                // Don't allow minor version change
+                minVersion = version.ToZeroVersion();
+                maxVersion = new Version(0, version.Minor.Value + 1, 0);
+            }
+            else
+            {
+                // Only patch non-zero, don't allow patch change
+                minVersion = version.ToZeroVersion();
+                maxVersion = new Version(0, 0, version.Patch.Value + 1);
+            }
+
+            return Tuple.Create(
+                    match.Length,
+                    minMaxComparators(minVersion, maxVersion));
+        }
+
+
+        private static Comparator[] minMaxComparators(Version minVersion, Version maxVersion,
+                Comparator.Operator maxOperator = Comparator.Operator.LessThan)
+        {
+            var minComparator = new Comparator(
+                    Comparator.Operator.GreaterThanOrEqual,
+                    minVersion);
+            if (maxVersion == null)
+            {
+                return new[] { minComparator };
+            }
+            else
+            {
+                var maxComparator = new Comparator(
+                        maxOperator, maxVersion);
+                return new[] { minComparator, maxComparator };
+            }
+        }
+    }
+}
---
+++ Testing34/CaretIntervalTest.cs
@@ -0,0 +1,66 @@
+﻿using NUnit.Framework;
+using SemVer;
+
+namespace Testing34
+{
+    class CaretIntervalTest
+    {
+
+        [Test]
+        public void TestCaretRangeContainVersion()
+        {
+            var range = new Range("^1.2.3");
+            Assert.IsTrue(range.Contains("1.2.3"));
+            Assert.IsTrue(range.Contains("1.9.9"));
+            Assert.IsFalse(range.Contains("2.0.0"));
+            Assert.IsFalse(range.Contains("1.2.2"));
+        }
+
+        [Test]
+        public void TestCaretRangeContainRange()
+        {
+            var range = new Range("^1.2.3");
+            var containRange = new Range(">=1.2.3 <2.0.0");
+            var notcontainRange = new Range(">=0.0.0 <1.2.3");
+            Assert.IsTrue(range.Contains(containRange));
+            Assert.IsFalse(range.Contains(notcontainRange)); 
+        }
+
+        [Test]
+        public void TestCaretrangeEqual1()
+        {
+            var range = new Range("^3.x");
+            var equalRange = new Range(">=3.0 <4.0");
+            Assert.IsTrue(range.Contains("3.2.9"));
+            Assert.IsTrue(range.Contains("3.0.0"));
+            Assert.IsFalse(range.Contains("2.9.0"));
+            Assert.IsTrue(range.Equals(equalRange));
+            Assert.IsTrue(range == equalRange);
+            Assert.IsFalse(range != equalRange);
+        }
+
+        [Test]
+        public void TestCaretRangeEqual2()
+        {
+            var range = new Range("^4");
+            var equalRange = new Range(">=4.0.0 <5.0");
+            Assert.IsTrue(range.Contains("4.1.4"));
+            Assert.IsTrue(range.Contains("4.1.9"));
+            Assert.IsFalse(range.Contains("1.2.4"));
+            Assert.IsTrue(range.Equals(equalRange));
+            Assert.IsTrue(range == equalRange);
+            Assert.IsFalse(range != equalRange);
+        }
+
+        [Test]
+        public void notEqualRanges()
+        {
+            var arange = new Range("^3");
+            var brange = new Range("^4");
+            Assert.IsFalse(arange.Equals(brange));
+            Assert.IsFalse(arange == brange);
+            Assert.IsTrue(arange != brange);
+        }
+
+    }
+}
